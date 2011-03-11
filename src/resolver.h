4a7009522863bfe35dc7de19a2cb88fbd9a71335
Vladimir Gorelov <virtual.lark@gmail.com>
2011-03-11 11:30:39 +0600

    Uploaded current version of sls.

---
+++ src/resolver.h
@@ -0,0 +1,9 @@
+#ifndef __resolver_h_
+#define __resolver_h_
+
+#include "types.h"	/* my_uint32_t */
+
+/* рудимент, искореню как будет время */
+int	resolve_to_uint32(char *url, my_uint32_t *ip);
+
+#endif
