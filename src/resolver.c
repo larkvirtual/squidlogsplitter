b62a9a07af459ce4c6768fce6ff0b3ab93e2c761
Vladimir Gorelov <virtual.lark@gmail.com>
2017-09-08 13:11:21 +0700

    Update copyright date, fix compiler warnings, MAX_NETWORKS doubled

--- src/resolver.c
+++ src/resolver.c
@@ -1,4 +1,4 @@
-/* Copyright (C) Vladimir <virtual.lark@gmail.com>, 2004-2011
+/* Copyright (C) Vladimir <virtual.lark@gmail.com>, 2004-2017
 
     SLS is free software; you can redistribute it and/or modify it under
     the terms of the GNU General Public License as published by the Free
